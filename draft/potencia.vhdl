ede06a99bf3a320febc6c8d14f1b7778a0fde812
Rodrigo Carvalho de Castro Leal <62775251+Raribaldo@users.noreply.github.com>
2023-07-18 16:24:18 -0300

    Ideia potencia.vhdl

--- draft/potencia.vhdl
+++ draft/potencia.vhdl
@@ -19,7 +19,8 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use ieee.numeric_std.all;
+use ieee.std_logic_arith.all;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -41,30 +42,16 @@ end potencia;
 
 architecture Behavioral of potencia is
 
-
-Component fourbitfa is
-    Port ( X : in  STD_LOGIC_VECTOR (3 downto 0);
-           Y : in  STD_LOGIC_VECTOR (3 downto 0);
-           C_in : in  STD_LOGIC;
-           C_out : out  STD_LOGIC;
-           Z : out  STD_LOGIC_VECTOR (3 downto 0)
-		   Flags :out STD_LOGIC_VECTOR (3 downto 0)
-		   );
-end component;
-
-signal c1, c2, c3 : STD_LOGIC;
+signal convertido : integer
+convertido <= conv_integer(unsigned(Yp))
 
 begin
+    process is
+    begin
+    Zp <= Zp sll convertido  
 
---fa0: fa port map(x(0), a(0), C_in, c1, z(0));
---fa1: fa port map(x(1), a(1), c1, c2, z(1));
---fa2: fa port map(x(2), a(2), c2, c3, z(2));
---fa3: fa port map(x(3), a(3), c3, C_out, z(3));
+    end process;
 
-fa0: fa port map(x(0), y(0), C_in, c1, z(0));
-fa1: fa port map(x(1), y(1), c1, c2, z(1));
-fa2: fa port map(x(2), y(2), c2, c3, z(2));
-fa3: fa port map(x(3), y(3), c3, C_out, z(3));
 
 end Behavioral;
 
